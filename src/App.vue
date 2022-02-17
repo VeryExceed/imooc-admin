ac27fbe4874e6a1e466fbff6c0c56f92b53edd5c
luomiao <1084459061@qq.com>
2022-02-18 07:09:40 +0800

    feat(git): add commitizen

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
