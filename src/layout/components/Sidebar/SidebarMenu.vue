7ddb62baface295b06fb7484a976a4d7e6b620bc
luomiao <1084459061@qq.com>
2022-03-02 08:22:02 +0800

    feat(Sidebar & Menu): feat: filter Routers

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -2,39 +2,38 @@
   <!-- 一级 menu 菜单 -->
   <el-menu
     :uniqueOpened="true"
-    default-active="2"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
+    :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText"
+    :active-text-color="$store.getters.cssVar.menuActiveText"
+    router
+    :default-active="activeMenu"
   >
     <!-- 子集 menu 菜单 -->
-    <el-submenu index="1">
-      <template #title>
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项 1</el-menu-item>
-      <el-menu-item index="1-2">选项 2</el-menu-item>
-    </el-submenu>
-    <!-- 具体菜单项 -->
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <template #title>导航四</template>
-    </el-menu-item>
+    <sidebar-item
+      v-for="item in routes"
+      :key="item.path"
+      :route="item"
+    ></sidebar-item>
   </el-menu>
 </template>
 
 <script setup>
 import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { filterRouters, generateMenus } from '@/utils/route'
-
+import SidebarItem from './SidebarItem.vue'
 const router = useRouter()
 const routes = computed(() => {
   const FilterRouters = filterRouters(router.getRoutes())
   return generateMenus(FilterRouters)
 })
-console.log(JSON.stringify(routes.value))
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
+})
+
+// console.log(JSON.stringify(routes.value))
 </script>
 
 <style lang="scss" scoped></style>
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,9 @@
+import variables from '@/styles/variables.scss'
+
 const getters = {
   token: (state) => state.user.token,
   userInfo: (state) => state.user.userInfo,
+  cssVar: (state) => variables,
   /**
    * true 表示已存在用户信息
    */
